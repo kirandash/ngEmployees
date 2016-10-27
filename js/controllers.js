9e17ad235aba62b2ac9dae42b7bfe45fd5650f84
kirandash <thekirankumardash@gmail.com>
2016-10-27 21:44:02 +0530

    03Dividing app in partials - 01 Set up deep linking

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,9 @@
-var myNameSpace = angular.module('myApp', []);
+var artistsControllers = angular.module('myApp', []);
 // Namespaces are used to protect variables
 // While myApp are used as module, they are different but mostly given same name
 
-myNameSpace.controller('MyController', ['$scope', '$http', function($scope, $http){
+// Note that controllers have a class based naming with first letters as capital
+artistsControllers.controller('ListController', ['$scope', '$http', function($scope, $http){
 	// MODEL is the data from database
 	$http.get("js/data.json").success(function(data){
 		$scope.artists = data;
